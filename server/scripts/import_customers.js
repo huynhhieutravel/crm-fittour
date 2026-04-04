dd8e5b7a193c8e35a167df2b6613e53947e0e488
Trong Hieu Huynh <tronghieuhuynh@gmail.com>
2026-04-04 20:30:54 +0700

    fix(script): Update dotenv path

--- server/scripts/import_customers.js
+++ server/scripts/import_customers.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
 const XLSX = require('xlsx');
 const bcrypt = require('bcryptjs');
 const db = require('../db');
