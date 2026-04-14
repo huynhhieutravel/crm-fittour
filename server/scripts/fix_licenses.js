7063b27528b50474e08c2570f83c999b0d51ac1d
Trong Hieu Huynh <tronghieuhuynh@gmail.com>
2026-04-14 21:42:33 +0700

    chore: add Truc Vy name back to VP doc

--- server/scripts/fix_licenses.js
+++ server/scripts/fix_licenses.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 async function main() {
   try {
-    const res = await pool.query("UPDATE licenses SET name = 'UỶ QUYỀN PHÓ GIÁM ĐỐC' WHERE name ILIKE '%VY%' RETURNING *");
+    const res = await pool.query("UPDATE licenses SET name = 'UỶ QUYỀN PHÓ GIÁM ĐỐC MS. TRÚC VY' WHERE id = 2 RETURNING *");
     console.log("Updated rows:", res.rows);
   } catch(e) {
     console.error(e);
